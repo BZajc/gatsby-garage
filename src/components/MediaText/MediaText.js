a7830ad76581f7b7c6182585be313f3963e9a7ca
BZajc <bbb.zaj@gmail.com>
2024-06-12 04:40:40 +0200

    Initial commit

---
+++ src/components/MediaText/MediaText.js
@@ -0,0 +1,41 @@
+import { GatsbyImage } from "gatsby-plugin-image";
+import React from "react";
+
+export const MediaText = ({
+  verticalAlignment,
+  style,
+  className,
+  mediaPosition,
+  gatsbyImage,
+  children,
+}) => {
+  const content = (
+    <div
+      className={`flex p-4 ${
+        verticalAlignment === "center" ? "items-center" : ""
+      }`}
+    >
+      <div>{children}</div>
+    </div>
+  );
+
+  return (
+    <div className={className} style={style}>
+      {mediaPosition === "right" ? (
+        <>
+          {content}
+          <div>
+            <GatsbyImage alt="" image={gatsbyImage} />
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <GatsbyImage alt="" image={gatsbyImage} />
+          </div>
+          {content}
+        </>
+      )}
+    </div>
+  );
+};
